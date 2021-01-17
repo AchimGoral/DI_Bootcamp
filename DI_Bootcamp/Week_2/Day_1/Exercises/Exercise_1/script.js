d1a48f1ddbf380ca19d6765d41a850cef42141a3
Achim Goral <a.goral@tu-bs.de>
2021-01-17 13:39:52 +0200

    JS Exercise_1

---
+++ DI_Bootcamp/Week_2/Day_1/Exercises/Exercise_1/script.js
@@ -0,0 +1,27 @@
+// 5 + "34"
+// 5 - "4"
+// 10 % 5
+// 5 % 10
+// "Java" + "Script"
+// " " + " "
+// " " + 0
+// true + true
+// true + false
+// false + true
+// false - true
+// 3 - 4
+// "Bob" - "bill"
+
+console.log(39);
+console.log(-1);
+console.log(0);
+console.log(5);
+console.log('JavaScript');
+console.log("  ");
+console.log(" 0");
+console.log(2);
+console.log(1);
+console.log(1);
+console.log(-1);
+console.log(1);
+console.log("NaN");
---
+++ DI_Bootcamp/Week_2/Day_1/Exercises/Test/script.js
@@ -0,0 +1,9 @@
+
+
+// ARRAYS
+
+let colors = ["blue", "red", "yellow", "pink", "green"];
+
+let isBoss = confirm("Are you the boss?");
+alert(isBoss);
+console.log(isBoss);
