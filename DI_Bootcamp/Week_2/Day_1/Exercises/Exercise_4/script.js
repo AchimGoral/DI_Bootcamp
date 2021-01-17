ba0e79153df115464edeb4edb4a149705c0a6ec6
Achim Goral <a.goral@tu-bs.de>
2021-01-17 14:57:06 +0200

    exercise_4 improved calc

--- DI_Bootcamp/Week_2/Day_1/Exercises/Exercise_4/script.js
+++ DI_Bootcamp/Week_2/Day_1/Exercises/Exercise_4/script.js
@@ -2,7 +2,29 @@ let num1 = prompt('Choose your first number');
 let number1 = parseInt(num1);
 let num2 = prompt('Choose your second number');
 let number2 = parseInt(num2);
-let result = number1+number2;
+let variable = prompt('Choose your second operation: +, -, *, /');
+let calc;
 
-console.log("Your result is: " + result);
-alert(`Your result is: ${result}`);
+if (variable == '+') {
+    calc = number1 + number2;
+    alert(`Your result is: ${calc}`);
+}
+
+else if (variable == '-') {
+    calc = number1 - number2;
+    alert(`Your result is: ${calc}`);
+}
+
+else if (variable == '*') {
+    calc = number1 * number2;
+    alert(`Your result is: ${calc}`);
+}
+
+else if (variable == '/') {
+    calc = number1 / number2;
+    alert(`Your result is: ${calc}`);
+}
+
+else {
+    alert(`This is not a valid calculation`);
+}
