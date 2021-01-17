2f51262287e4061bdc90fc3c6dad5700245eb796
Achim Goral <a.goral@tu-bs.de>
2021-01-17 14:49:57 +0200

    Exercise_2

---
+++ DI_Bootcamp/Week_2/Day_1/Exercises/Exercise_4/script.js
@@ -0,0 +1,8 @@
+let num1 = prompt('Choose your first number');
+let number1 = parseInt(num1);
+let num2 = prompt('Choose your second number');
+let number2 = parseInt(num2);
+let result = number1+number2;
+
+console.log("Your result is: " + result);
+alert(`Your result is: ${result}`);
