d51ec3a3b41be67fd01219df2381aecf40639256
Achim Goral <a.goral@tu-bs.de>
2021-01-18 14:25:35 +0200

    Exercise_1

--- DI_Bootcamp/Week_2/Day_2/Exercise_1/script.js
+++ DI_Bootcamp/Week_2/Day_2/Exercise_1/script.js
@@ -11,3 +11,32 @@ if ( x > y) {
 
 // Exercise_2
 
+let newDog = "Chihuahua";
+
+console.log(newDog.length);
+
+// Exercise_3
+
+// let z = prompt("Choose your integer");
+
+// if (z%2 == 0) {
+//     alert(`${z} is an even number`);
+// } else {
+//     alert(`${z} is an odd number`);
+// }
+
+// Exercise_4
+
+let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+
+console.log(users);
+
+if (users.length == 0) {
+    console.log('no one is online');
+} else if (users.length == 1) {
+    console.log(`${users[0]} is online`);
+} else if (users.length == 2) {
+    console.log(`${users[0]} and ${users[1]} are online`);
+} else {
+    console.log(`${users[0]} and ${users[1]} and ${users.length-2} are online`);
+}
--- DI_Bootcamp/Week_2/Day_2/Test/script.js
+++ DI_Bootcamp/Week_2/Day_2/Test/script.js
@@ -36,11 +36,58 @@
 
 // Conditionals
 
-let bankAmount = 500;
-let acccountNumber = 1234;
+// let bankAmount = 500;
+// let acccountNumber = 1234;
 
-if (bankAmount >= 500 && acccountNumber == 1234) {
-    console.log('I can buy a computer');
+// if (bankAmount >= 500 && acccountNumber == 1234) {
+//     console.log('I can buy a computer');
+// } else {
+//     console.log("I can`t buy a computer");
+// }
+
+let age = prompt("what is your age?");
+
+if (age < 18) {
+    alert("Sorry, you are too young to drive this car. Powering off");
+} else if (age == 18) {
+    alert("Congratulations on your first year of driving. Enjoy the ride!");
 } else {
-    console.log("I can`t buy a computer");
+    alert("Powering On. Enjoy the ride!");
+}
+
+// Ex_2
+
+let a = 2 + 2;  //a equals 4
+
+switch (a) {
+  case 3:       // checked and false, skipped to the next one
+    alert( 'Too small' );
+    break;
+   case 4:      // checked and true
+    alert( 'Exactly!' );    //Exactly! will be printed out
+    break;  //We are leaving the switch case
+  case 5:
+    alert( 'Too large' );
+    break;
+  default:
+    alert( "I don't know such values" );
+}
+
+// Ex_3
+
+let b = 2 + 2;      // b equals 4
+
+switch (b) {
+  case 4:           // Statement is true
+    alert('Right!');    //Right! will be printed
+    break;          // Leaveing the switch statement
+
+  case 3: // (*) grouped two cases (Doesn't mater for us)
+  case 5:
+    alert('Wrong!');
+    alert("Why don't you take a math class?");
+    break;
+
+  default:
+    alert('The result is strange. Really.');
 }
