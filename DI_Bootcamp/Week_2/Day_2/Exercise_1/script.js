8f8e5d46523a356492746c9410cf149dba3ce6be
Achim Goral <a.goral@tu-bs.de>
2021-01-18 11:57:30 +0200

    Folder replacement and daily exercise

---
+++ DI_Bootcamp/Week_2/Day_2/Exercise_1/script.js
@@ -0,0 +1,13 @@
+// Exercise_1
+
+let x = 5;
+let y = 2;
+
+if ( x > y) {
+    console.log("X is the biggest number");
+} else {
+    console.log("y is the biggest number");
+}
+
+// Exercise_2
+
---
+++ DI_Bootcamp/Week_2/Day_2/Test/script.js
@@ -0,0 +1,46 @@
+// let colors = ['red', 'blue', 'yellow'];
+
+// let user = {
+//     username : "John",
+//     password : 1234,
+//     email : "jondoe@gmail.com",
+//     LogIn : true,
+//     countries : ["Israel", "America"],
+//     friends : {
+//         name : ["David", "Sarah"]
+//     }
+// };
+
+// let users = [
+//     {
+//         username : "John",
+//         password : 1234
+//     },
+//     {
+//         username : "Lea",
+//         password : 2222
+//     },
+//     {
+//         username : "David",
+//         password : 6767
+//     }
+// ];
+
+// for (const n in users) {
+//     console.log(users[n]);
+// }
+
+// for (const n in users) {
+//     console.log(users[n].password);
+// }
+
+// Conditionals
+
+let bankAmount = 500;
+let acccountNumber = 1234;
+
+if (bankAmount >= 500 && acccountNumber == 1234) {
+    console.log('I can buy a computer');
+} else {
+    console.log("I can`t buy a computer");
+}
