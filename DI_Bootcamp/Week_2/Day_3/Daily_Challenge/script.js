db0c168b64ed5e6396f1fa8e842dd2b602b0b419
Achim Goral <a.goral@tu-bs.de>
2021-01-19 20:41:11 +0200

    Daily Challenge

---
+++ DI_Bootcamp/Week_2/Day_3/Daily_Challenge/script.js
@@ -0,0 +1,13 @@
+// Daily_Challenge
+let list = "";
+
+let i, j = 0;
+
+for (i = 0; i < 3; i++) {
+    for (j = 0; j < 2; j++) {
+        list += "*";
+        console.log(list);
+    }
+}
+
+// I don't know if that was requested, but it looks similar
