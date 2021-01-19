7b7de64da10ec83ffbbe9e072aad5475956666b7
Achim Goral <a.goral@tu-bs.de>
2021-01-19 10:06:15 +0200

    Daily Challenge modifications

--- DI_Bootcamp/Week_2/Day_2/Daily_Challenge/script.js
+++ DI_Bootcamp/Week_2/Day_2/Daily_Challenge/script.js
@@ -2,16 +2,12 @@ let sentence = prompt("Write a sentence with not first and bad later. Or a sente
 
 let searchNot = sentence.search("not");
 let searchBad = sentence.search("bad");
-
-
 let good = "good";
 
-if (searchNot == -1 && searchBad == -1) {
-    alert(sentence);
-} else if (searchNot > searchBad) {
+if ((searchNot == -1 && searchBad == -1) || (searchNot > searchBad)) {
     alert(sentence);
 } else {
-    let midSent1 = sentence.slice(0, searchNot);
-    let finalSent = midSent1.concat(good);
+    let finalSent = sentence.slice(0, searchNot).concat(good)+sentence.slice(searchBad+3);
     alert(finalSent);
 }
+// The movie was not that bad, I liked it eventually
