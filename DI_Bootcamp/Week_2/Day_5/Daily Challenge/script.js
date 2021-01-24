dda4069e72825536ea29aae1539a658cd9ed6878
Achim Goral <a.goral@tu-bs.de>
2021-01-24 10:05:31 +0200

    Changes to daily Challenge

--- DI_Bootcamp/Week_2/Day_5/Daily Challenge/script.js
+++ DI_Bootcamp/Week_2/Day_5/Daily Challenge/script.js
@@ -1,27 +1,30 @@
-let sumStr = prompt(`How many bottels of beer?`);
-let sum = parseInt(sumStr);
+let sum = prompt(`How many bottels of beer?`);
 
-let count = 1; // COunter for beer
+while (isNaN(sum) == true) {        // Check for Int
+    sum = prompt(`How many bottels of beer?`);
+}
 
-// let sentence_1 = (`${sum} bottels of beer on the wall`);
-// let sentence_2 = (`${sum} bottels of beer`);
-// let sentence_3 = (`Take ${count} down, pass it around`);
-// let sentence_4 = (`Take ${count} down, pass them around`); // SOmehow not working, when passed into the logs, any suggestions?
+let count = 1; // Counter for beer
 
 console.log("===============================");
 while(sum > 1) {
+
     console.log(`${sum} bottels of beer on the wall`);
     console.log(`${sum} bottels of beer on the wall`);
     console.log(`${sum} bottels of beer`);
-    if (count == 1) {
+
+    if (count == 1) {               // One-time statement for the first bottle
         console.log(`Take ${count} down, pass it around`);
+    } else if (count > sum) {       // If the amount of bottles to take down is bigger than the sum, take down the sum
+        console.log(`Take ${sum} down, nothing to pass around`);
     } else {
         console.log(`Take ${count} down, pass them around`);
     }
+    
     sum -= count;
     count++;
 }
 
 console.log(`0 bottels of beer on the wall`);
-console.log(`That's it go home`);
+console.log(`Go home, you're drunk!!!`);
 console.log("===============================");
