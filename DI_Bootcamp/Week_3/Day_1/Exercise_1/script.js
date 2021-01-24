f87d622425aec04570cc291f1d067d383622f14a
Achim Goral <a.goral@tu-bs.de>
2021-01-24 15:36:45 +0200

    Exercise_1 Task 2

--- DI_Bootcamp/Week_3/Day_1/Exercise_1/script.js
+++ DI_Bootcamp/Week_3/Day_1/Exercise_1/script.js
@@ -1,23 +1,43 @@
-//  Exercise_1
+// //  Exercise_1
+
+// let bodyWebsite = document.body;
+
+// let newNavBar = bodyWebsite.firstElementChild.setAttribute("id", "socialNetworkNavigation");
+
+// let newLiTag = document.createElement("li");
+
+// let newContent = document.createTextNode("Logout");
+
+// let divTag = bodyWebsite.firstElementChild;
+
+// let ulTag = divTag.firstElementChild;
+
+// let liTag = ulTag.firstElementChild;
+
+// newLiTag.appendChild(newContent);
+
+// ulTag.appendChild(newLiTag);
+
+
+// // Bonus Exercise_1
+
+// for (let i = 0; i < 6; i++) {
+//     console.log(ulTag.children[i].textContent);
+// }
+
+// Exercise_2
 
 let bodyWebsite = document.body;
 
-let newNavBar = bodyWebsite.firstElementChild.setAttribute("id", "socialNetworkNavigation");
+let ulTag_1 = bodyWebsite.children[1];
 
-let newLiTag = document.createElement("li");
+let liTag_2 = ulTag_1.children[1];
 
-let newContent = document.createTextNode("Logout");
+liTag_2.innerHTML = "Richard";
 
-let divTag = bodyWebsite.firstElementChild;
+let ulTag_item1 = document.getElementsByClassName("list");
 
-let ulTag = divTag.firstElementChild;
-
-let liTag = ulTag.firstElementChild;
-
-newLiTag.appendChild(newContent);
-
-ulTag.appendChild(newLiTag);
-
-for (let i = 0; i < 6; i++) {
-    console.log(ulTag.children[i].textContent);
+for (let i = 0; i < ulTag_item1.length; i++) {
+    ulTag_item1[i].firstElementChild.innerHTML = "Achim";
 }
+
