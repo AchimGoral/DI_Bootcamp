ef9d33d03751f2101dfe200af6e0f2fc83ec1e27
Achim Goral <achimgoral.il@gmail.com>
2021-01-26 15:25:04 +0200

    daily Challenge

---
+++ DI_Bootcamp/Week_3/Day_3/Daily Challenge/script.js
@@ -0,0 +1,15 @@
+let container = document.getElementById('text-container');
+let input = document.getElementById('text-input');
+
+input.addEventListener('keypress', keyLogger);
+
+let letters = /^[A-Za-z]+$/;
+
+function keyLogger(element) {
+    if (element.key.match(letters)){
+        container.textContent += ` ${element.key}`;
+        console.log(element);
+    } else {
+        container.textContent += ``;
+    }
+}
