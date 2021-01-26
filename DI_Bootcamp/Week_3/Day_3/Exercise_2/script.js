56e98178f13c0b17cd7e2c3264b49e0bd117e6aa
Achim Goral <achimgoral.il@gmail.com>
2021-01-26 14:37:23 +0200

    Exercises

---
+++ DI_Bootcamp/Week_3/Day_3/Exercise_2/script.js
@@ -0,0 +1,13 @@
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function dragStart(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
+}
