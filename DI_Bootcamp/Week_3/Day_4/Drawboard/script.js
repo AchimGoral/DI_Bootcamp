0684f97c3165b5054eaa29e4ab3ea5b368567453
Achim Goral <achimgoral.il@gmail.com>
2021-01-27 12:21:32 +0200

    coding project update

--- DI_Bootcamp/Week_3/Day_4/Drawboard/script.js
+++ DI_Bootcamp/Week_3/Day_4/Drawboard/script.js
@@ -2,33 +2,57 @@
 for (let i = 0; i < (80*48); i++) {
     let divContainer = document.createElement('div');
     document.getElementById('gridPad').appendChild(divContainer);
-    divContainer.style.backgroundColor = "black";
+    divContainer.style.backgroundColor = "grey";
 }           // Creating the grid-layout with a field size of 80*48
 
 let colorDiv = null; // Empty variable for color in div
 let mousedown = false; // Boolean for mousedown event
+let sidebar_color; // initialising element for loop
+let grid_fill; // initialising element for loop
 let clear = document.getElementById('button-reset');
-let sidebar_color = document.querySelectorAll('#sidebar > div');
-let grid_fill = document.querySelectorAll('#gridPad > div');
+let sidebar_colors = document.querySelectorAll('#sidebar > div');
+let grid_fills = document.querySelectorAll('#gridPad > div');
 let body = document.getElementsByTagName('body')[0];
+
+clear.addEventListener("click", clearFunction); // clears the creen by clicking the button and calling the function bellow
+
+function clearFunction () {
+    for (grid_fill of grid_fills) {
+        grid_fill.style.backgroundColor = "grey";
+    }
+}
 
 body.addEventListener("mousedown", function(){
     mousedown = true;
-});
+}); // checks for boolean of mouse down status
 
 body.addEventListener("mouseup", function(){
     mousedown = false;
-});
+}); // checks for boolean of mouse down status
 
-for(sidebar_color of sidebar_color) {
-    sidebar_color.addEventListener("click", function(e){
-        colorDiv = e.target.style.backgroundColor;
-        console.log(colorDiv);
-    });
+for(sidebar_color of sidebar_colors) {
+    sidebar_color.addEventListener("click", sideColorFunction);
+} // selects the color of the sidebar
+
+function sideColorFunction (e) {
+    colorDiv = e.target.style.backgroundColor;
+    console.log(colorDiv);
 }
 
-clear.addEventListener("click", function(){
-    for (grid_fill of grid_fill) {
-        grid_fill.style.backgroundColor = "white";
+
+for(grid_fill of grid_fills) {
+    grid_fill.addEventListener("mousedown", fillColorFunction_click);
+    grid_fill.addEventListener("mouseover", fillColorFunction_move);
+}
+
+function fillColorFunction_click (e) {
+    if (colorDiv != null) {
+        e.target.style.backgroundColor = colorDiv;
     }
-});
+}
+
+function fillColorFunction_move (e) {
+    if(mousedown == true && colorDiv != null) {
+        e.target.style.backgroundColor = colorDiv;
+    }
+}
