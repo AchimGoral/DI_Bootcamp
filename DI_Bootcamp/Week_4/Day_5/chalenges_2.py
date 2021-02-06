f4659568da648ef7e1097e77f648533974f372bc
Achim Goral <achimgoral.il@gmail.com>
2021-02-06 11:07:01 +0200

    'Mini-project'

---
+++ DI_Bootcamp/Week_4/Day_5/chalenges_2.py
@@ -0,0 +1,30 @@
+# # Exercise_1
+
+# sandwich_orders = ['Hamburger', 'Tuna-Sandwich', 'Tomato-Mozarella-Sandwich', 'Egg-Sandwich']
+
+# finished_sandwiches = []
+
+# for item in sandwich_orders:
+#     print(f"I made your {item}")
+#     finished_sandwiches.append(item)
+
+# for item in finished_sandwiches:
+#     print(f"Your {item} was made")
+
+# # Exercise_2
+
+# sandwich_orders = ['Hamburger', 'Pastrami-Sandwich', 'Tuna-Sandwich', 'Tomato-Mozarella-Sandwich', 'Pastrami-Sandwich' 'Egg-Sandwich', 'Pastrami-Sandwich']
+
+# finished_sandwiches = []
+
+# print("We ran out of Pastrami!")
+
+# while i in sandwich_orders:
+#     if 'Pastrami-Sandwich' in sandwich_orders:
+#         i += 1
+#         continue
+#     else:
+#         finished_sandwiches.append(item)
+#         i += 1
+
+# print(finished_sandwiches)
--- DI_Bootcamp/Week_4/Day_5/mini_project_1.py
+++ DI_Bootcamp/Week_4/Day_5/mini_project_1.py
@@ -1,49 +1,28 @@
-board = [' ', ' ', ' ', 
-         ' ', ' ', ' ', 
-         ' ', ' ', ' ']
 
-# Flag for win: checks for winning condition
-win = False
-# counts the games with win check after 5 and ends with 9 moves
-game_counter = 0
+def display_board(game_board):
 
-
-def display_board():
-
-    print("")
-    print("Tic Tac Toe")
     print("")
     print("*************")
-    print(f"* {board[0]} | {board[1]} | {board[2]} *")
+    print(f"* {board[6]} | {board[7]} | {board[8]} *")
     print("*-----------*")
     print(f"* {board[3]} | {board[4]} | {board[5]} *")
     print("*-----------*")
-    print(f"* {board[6]} | {board[7]} | {board[8]} *")
+    print(f"* {board[0]} | {board[1]} | {board[2]} *")
     print("*************")
     print("")
 
-def play():
-
-    global game_counter
-    player_1 = 'X'
-    player_2 = 'O'
-
-    # Display the board initialy
-    display_board()
-    # Player input is checked
-    player_input()
-    #After player input, game_counter gets a raise
-    game_counter += 1
-    display_board()
-
 
 def player_input():
-    global board
-    position = input("Where do want to make your input? (1-9): ")
+
+    position = int(input("Where do want to make your input? (1-9): "))
+    position -= 1 # Shift to 0-8 like Board numbers
+    return position
+
 
 def check_win():
-    #GLobal variable win is called
-    global win
+
+    win = False
+
     # Horizontal
     if board[0] == board[1] and board[1] == board[2] and board[0] != ' ':
         win = True
@@ -51,6 +30,7 @@ def check_win():
         win = True
     elif board[6] == board[7] and board[7] == board[8] and board[6] != ' ':
         win = True
+
     # Vertical
     elif board[6] == board[3] and board[3] == board[0] and board[6] != ' ':
         win = True
@@ -58,6 +38,7 @@ def check_win():
         win = True
     elif board[8] == board[5] and board[5] == board[2] and board[8] != ' ':
         win = True
+
     # Diagonal
     elif board[0] == board[4] and board[4] == board[8] and board[0] != ' ':
         win = True
@@ -67,4 +48,57 @@ def check_win():
         win = False
     return win
 
-play()
+
+def play():
+
+    board = [' ', ' ', ' ', 
+             ' ', ' ', ' ', 
+             ' ', ' ', ' ']
+    
+    # counts the games with win check after 5 and ends with 9 moves
+    game_counter = 9
+
+    # Initialising player
+    player = ''
+    
+    # Check for win
+    win_bool = check_win()
+
+    #While loop for counting down the games
+    while game_counter > 0:
+        # checks by modulo, if player 1 or player 2 is playing
+        if game_counter%2 != 0:
+            player = 'X'
+            print("Player 1 plays")
+        else:
+            player = 'O'
+            print("Player 2 plays")
+        # Display the board initialy
+        display_board(game_board)
+
+        #Player input is checked
+        position = player_input()
+
+        while board[position] != ' ':
+            print("Can't place over an occupied field\n")
+            position = player_input()
+
+        else:
+            board[position] = player
+
+        display_board(game_board)
+
+        if game_counter <= 5:
+            win_bool
+            print(win_bool)
+            if win_bool:
+                print("Congratulations")
+                return
+            else:
+                pass
+        #After player input, game_counter gets a count down
+        game_counter -= 1
+    else:
+        print("Game over. It's a tie")
+
+play() # calling the play function to start the game
