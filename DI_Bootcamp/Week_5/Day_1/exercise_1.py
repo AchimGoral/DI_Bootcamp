585f4490af3e74f2f37d30d80379be3a5e2f7da3
Achim Goral <achimgoral.il@gmail.com>
2021-02-07 16:32:05 +0200

    Exercise_1

--- DI_Bootcamp/Week_5/Day_1/exercise_1.py
+++ DI_Bootcamp/Week_5/Day_1/exercise_1.py
@@ -6,117 +6,134 @@ class Cat:
         self.name = name
         self.age = age
 
-    def oldest_cat(self):
-        temp = 0
-        if self.age > temp:
-            temp = self.age
-        print(f"The oldest cat is {self.name} and is {self.age} years old")
 
 c1 = Cat("Dona", 15)
 c2 = Cat("Marry", 2)
 c3 = Cat("Tom", 10)
-c1.oldest_cat()
-c2.oldest_cat()
-c3.oldest_cat()
 
-# # Exercise_2
+cats = [c1, c2, c3]
 
-# class Dog:
+def oldest_cat():
+    temp = 0
+    name = ""
+    for cat in cats:
+        if cat.age > temp:
+            temp = cat.age
+            name = cat.name
+    print(f"The oldest cat is {name} and is {temp} years old")
 
-#     def __init__(self, name, height):
-#         self.name = name
-#         self.height = height
+oldest_cat()
 
-#     def bark(self):
-#         print(f"goes woof")
+# Exercise_2
 
-#     def jump(self, height):
-#         height *=2
-#         print(f"jumps {self.height}cm high")
+class Dog:
 
-# davids_dog = Dog("Rex", 50)
-# print(davids_dog.name, davids_dog.height)
-# sarahs_dog = Dog("Teacup", 20)
-# print(sarahs_dog.name, sarahs_dog.height)
+    def __init__(self, name, height):
+        self.name = name
+        self.height = height
 
-# if davids_dog.height > sarahs_dog.height:
-#     print(f"{davids_dog.name} is bigger")
-# else:
-#     print(f"{sarahs_dog.name} is bigger")
+    def bark(self):
+        print(f"goes woof")
 
-# # Exercise_3
+    def jump(self, height):
+        height *=2
+        print(f"jumps {self.height}cm high")
 
-# class Song:
+davids_dog = Dog("Rex", 50)
+print(davids_dog.name, davids_dog.height)
+sarahs_dog = Dog("Teacup", 20)
+print(sarahs_dog.name, sarahs_dog.height)
 
-#     def __init__(self, lyrics):
-#         self.lyrics = lyrics
+if davids_dog.height > sarahs_dog.height:
+    print(f"{davids_dog.name} is bigger")
+else:
+    print(f"{sarahs_dog.name} is bigger")
 
-#     def sing_me_a_song(self):
-#         for lines in self.lyrics:
-#             print(lines)
+# Exercise_3
 
+class Song:
 
-# stairway= Song(["There’s a lady who's sure","all that glitters is gold", "and she’s buying a stairway to heaven"])
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
 
-# stairway.sing_me_a_song()
-
-# # Exercise_4
-
-# class Zoo:
-
-#     def __init__(self, zoo_name):
-#         self.animals = []
-#         self.name = zoo_name
-#         self.new_dict = {}
+    def sing_me_a_song(self):
+        for lines in self.lyrics:
+            print(lines)
 
 
-#     def add_animal(self, new_animals):
-#         print(f"Which animal should we add to the zoo --> {new_animals}")
-#         if new_animals not in self.animals:
-#             self.animals.append(new_animals)
-#         print(self.animals)
+stairway= Song(["There’s a lady who's sure","all that glitters is gold", "and she’s buying a stairway to heaven"])
+
+stairway.sing_me_a_song()
+
+# Exercise_4
+
+class Zoo:
+
+    def __init__(self, zoo_name):
+        self.animals = []
+        self.name = zoo_name
+        self.new_dict = {}
 
 
-#     def get_animal(self, animals):
-#         for animal in self.animals:
-#             print(f"{animal}")
+    def add_animal(self, new_animals):
+        print(f"Which animal should we add to the zoo --> {new_animals}")
+        if new_animals not in self.animals:
+            self.animals.append(new_animals)
+        print(self.animals)
 
 
-#     def sell_animal(self, animal_sold):
-#         print(f"{animal_sold} gets sold")
-#         if animal_sold in self.animals:
-#             self.animals.remove(animal_sold)
-#         print(f"Unsorted list of animals: {self.animals}")
+    def get_animal(self, animals):
+        for animal in self.animals:
+            print(f"{animal}")
 
 
-#     def sort_animals(self):
-#         self.animals.sort()
-#         print(f"Sorted List of animals: {self.animals}")
-#         for i in range(len(self.animals)):
-#             case = {i+1: self.animals[i]}
-#             self.new_dict.update(case)
-#         print(self.new_dict)
+    def sell_animal(self, animal_sold):
+        print(f"{animal_sold} gets sold")
+        if animal_sold in self.animals:
+            self.animals.remove(animal_sold)
+        print(f"Unsorted list of animals: {self.animals}")
 
 
-#     def get_groups(self):
-#         for item in self.new_dict.items():
-#             print(f"list of animals: {item}")
+    def sort_animals(self):
+        new_list = []
+        self.animals.sort()
+        print(f"Sorted List of animals: {self.animals}")
+        for animal in self.animals:
+            if not new_list:
+                new_list.append([animal])
+            else:
+                added = False
+                for sub_list in new_list:
+                    if sub_list[0][0] == animal[0]:
+                        sub_list.append(animal)
+                        added = True
+                if not added:
+                    new_list.append([animal])
+        for index, sub_list in enumerate(new_list):
+            self.new_dict.update({index+1:", ".join(sub_list)})
+        print(new_list)
+
+
+    def get_groups(self):
+        for item in self.new_dict.items():
+            print(f"list of animals: {item}")
 
 
 
-# ramat_gan_safari = Zoo("Ramat Gan")
-# # Adding animals
-# ramat_gan_safari.add_animal("Giraffe")
-# ramat_gan_safari.add_animal("Chimpansee")
-# ramat_gan_safari.add_animal("Orang")
-# ramat_gan_safari.add_animal("Penguin")
-# ramat_gan_safari.add_animal("Anaconda")
-# ramat_gan_safari.add_animal("Python")
-# # Get a specific animal from the list
-# ramat_gan_safari.get_animal("Orang")
-# # Remove animal from the list
-# ramat_gan_safari.sell_animal("Giraffe")
-# # Sort the animals and put them in a dict
-# ramat_gan_safari.sort_animals()
-# # Print the different dict groups
-# ramat_gan_safari.get_groups()
+ramat_gan_safari = Zoo("Ramat Gan")
+# Adding animals
+ramat_gan_safari.add_animal("Giraffe")
+ramat_gan_safari.add_animal("Ape")
+ramat_gan_safari.add_animal("Chimpansee")
+ramat_gan_safari.add_animal("Orang")
+ramat_gan_safari.add_animal("Penguin")
+ramat_gan_safari.add_animal("Anaconda")
+ramat_gan_safari.add_animal("Python")
+# Get a specific animal from the list
+ramat_gan_safari.get_animal("Orang")
+# Remove animal from the list
+ramat_gan_safari.sell_animal("Giraffe")
+# Sort the animals and put them in a dict
+ramat_gan_safari.sort_animals()
+# Print the different dict groups
+ramat_gan_safari.get_groups()
