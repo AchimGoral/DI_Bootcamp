0557cad9ba9b791c7ba12542849722827378c96f
Achim Goral <achimgoral.il@gmail.com>
2021-02-10 14:57:39 +0200

    exercise_1

---
+++ DI_Bootcamp/Week_5/Day_4/exercise_1.py
@@ -0,0 +1,61 @@
+# # Exercise_1
+
+# import json
+# import random as rd
+# from cprint import cprint
+
+
+# def get_words_from_file():
+#     with open('sowpods.txt', 'r') as f:
+#         data = f.readlines()
+#         return data
+
+
+# def get_random_sentence(lenght):
+#     data = get_words_from_file()
+#     sentence = rd.sample(data, lenght)
+#     sentence = map(lambda s: s.strip(), sentence)
+#     sent_string = ''
+#     sent_string += ' '.join(sentence).title()
+#     print(sent_string)
+
+
+# def main():
+#     print("This Programm lets you pick a sentence lenght between 2 and 20 words to print out a randaom sentence")
+
+#     lenght = int(input("Pick a number between 2 and 20: "))
+
+#     while lenght < 2 or lenght > 20:
+#         print("Your number is too small or too big. Try again...")
+#         lenght = int(input("Pick a number between 2 and 20: "))
+    
+#     get_words_from_file()
+
+#     get_random_sentence(lenght)
+
+# main()
+
+# Exercise_2
+
+import json
+
+sampleJson = """{ 
+   "company":{ 
+      "employee":{ 
+         "name":"emma",
+         "payable":{ 
+            "salary":7000,
+            "bonus":800
+         }
+      }
+   }
+}"""
+
+data = json.loads(sampleJson)
+
+print(data['company']['employee']['payable']['salary'])
+
+data['company']['employee']['birth_date'] = '03/12/1991'
+
+with open('exercise.json', 'w') as f:
+    json.dump(data, f, indent=2)
--- DI_Bootcamp/Week_5/Day_4/test.py
+++ DI_Bootcamp/Week_5/Day_4/test.py
@@ -24,4 +24,46 @@
 
 # JSON
 
+import json
 
+# load      read json from a file convert to python
+# loads     string: convert a json string to python
+
+# dump      convert from python to json and write to file
+# dumps     convert from python to a sjson string
+
+# thing = [
+#     {
+#         'name': 'bob',
+#         'age': 55,
+#         'stuff': True
+#     },
+#     {
+#         'nums': [1,2,3],
+#         'letters': ['a', 'b', 'c']
+#     },
+# ]
+
+# with open('jfile.txt', 'w') as f:
+#     json.dump(thing, f, indent=2)
+
+# with open('jfile.txt', 'r') as f:
+#     thing2 = json.load(f)
+
+# Requests get and post
+
+import requests
+import json
+import time
+
+# resp = requests.get("http://api.open-notify.org/iss-now")
+
+
+# data = resp.json()
+
+for _ in range(10):
+    resp = requests.get("http://api.open-notify.org/iss-now")
+    data = resp.json()
+    print(data['iss_position']['latitude'], data['iss_position']['longitude'])
+    time.sleep(1)
+    
