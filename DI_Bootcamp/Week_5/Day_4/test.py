d96a693c05190dd62bc532e0f560751874d826e7
Achim Goral <achimgoral.il@gmail.com>
2021-02-10 11:05:48 +0200

    new data

---
+++ DI_Bootcamp/Week_5/Day_4/test.py
@@ -0,0 +1,27 @@
+# # I/O files
+
+# with open("test.txt", "w") as f:
+#     f.write("adam@gmail.com\n")
+
+# with open("test.txt", "a") as f:
+#     f.write("Mike@ramsey.com\n")
+
+# with open("test.txt", "a") as f:
+#     f.write("Business@gmail.com\n")
+
+# with open("test.txt", "a") as f:
+#     f.write("Tom@yahoo.com\n")
+
+# with open("test.txt", "a") as f:
+#     f.write("Hello@gmail.com\n")
+
+# with open("test.txt", "r") as f:
+#     data = f.readlines()
+
+# for person in data:
+#     person = person.strip('\n')
+#     print(f"sending email to {person}")
+
+# JSON
+
+
