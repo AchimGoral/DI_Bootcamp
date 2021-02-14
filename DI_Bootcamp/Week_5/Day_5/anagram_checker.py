0353f588ef16968329da6fb7b687d4766eb6f0df
Achim Goral <achimgoral.il@gmail.com>
2021-02-14 14:15:09 +0200

    Ex_1 14.02

--- DI_Bootcamp/Week_5/Day_5/anagram_checker.py
+++ DI_Bootcamp/Week_5/Day_5/anagram_checker.py
@@ -4,4 +4,11 @@ class AnagramChecker:
         self.new_list = []
         with open("text_file.txt", "r") as f:
             self.new_list = f.read().splitlines()
-        print(self.new_list)
+    
+
+    def is_valid_word(self, word):
+        for word in self.new_list:
+            if word in self.new_list:
+                return True
+            else: 
+                return False
