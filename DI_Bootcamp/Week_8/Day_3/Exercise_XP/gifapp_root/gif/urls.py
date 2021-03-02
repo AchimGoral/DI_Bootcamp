c5921e138fa96ce9ace40d5cd5e4bfdc721f5783
Achim Goral <achimgoral.il@gmail.com>
2021-03-02 20:15:52 +0200

    chnages ex_1

--- DI_Bootcamp/Week_8/Day_3/Exercise_XP/gifapp_root/gif/urls.py
+++ DI_Bootcamp/Week_8/Day_3/Exercise_XP/gifapp_root/gif/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('animal/<int:animal_id>', views.animal, name='animal'),
+    path('', views.main, name='main'),
+    path('gif_show/<int:gif_id>', views.gif_show, name='gif_show'),
+    path('', views.categories, name='categories'),
 ]
--- DI_Bootcamp/Week_8/Day_3/Exercise_XP/gifapp_root/gif/views.py
+++ DI_Bootcamp/Week_8/Day_3/Exercise_XP/gifapp_root/gif/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import * # Gif(mtm), Category
 
-# Create your views here.
+
+def main(request):
+    gifs = Gif.objects.all()
+    context = {'gifs': gifs}
+    return render(request, 'main.html', context)
+
+def gif_show(request, gif_id):
+    gifs = Gif.objects.get(id=gif_id)
+    context = {'gifs': gifs}
+    return render(request, 'gif_show.html', context)
+
+def categories(request):
+    pass
+    # cat = Categories.objects.all()
+    # context = {'category': cat}
+    # return render(request, 'categories.html', context)
