60e675e6b200f9d4c306562bf374eaeabc36fd54
Achim Goral <achimgoral.il@gmail.com>
2021-03-03 18:33:47 +0200

    Changes ex_1

--- DI_Bootcamp/Week_8/Day_4/Exercise_XP/todo_project_root/todo_app/models.py
+++ DI_Bootcamp/Week_8/Day_4/Exercise_XP/todo_project_root/todo_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100, blank=False)
+    details = models.TextField()
+    has_been_done = models.BooleanField(null=True, default=False)
+    date_creation = models.DateTimeField(auto_now_add=True)
+    date_completion = models.DateTimeField(auto_now=True, null=False)
+    deadline_date = models.DateTimeField(null=False)
+
+    def __repr__(self):
+        return f"ID: {self.id} Title: {self.title}"
+
+    def __str__(self):
+        return f"ID: {self.id} Title: {self.title}"
+
+class Category(models.Model):
+    todos = models.ManyToManyField(Todo, related_name='all_todo')
+    name = models.CharField(max_length=100, default='no category')
+    image = models.URLField(max_length=200)
+
+    def __repr__(self):
+        return f"ID: {self.id} Category: {self.name}"
+
+    def __str__(self):
+        return f"ID: {self.id} Category: {self.name}"
--- DI_Bootcamp/Week_8/Day_4/Exercise_XP/todo_project_root/todo_app/views.py
+++ DI_Bootcamp/Week_8/Day_4/Exercise_XP/todo_project_root/todo_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def todo_view(request):
+    pass
+
+def display_todo(request):
+    pass
